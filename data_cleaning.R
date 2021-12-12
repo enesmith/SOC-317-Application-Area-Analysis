dd19dd039a628a042c85954b4283f9da8d8e8e2f
Elisabeth Nesmith <enesmith@smith.edu>
2021-12-11 19:40:47 -0500

    switching data and loading, checking for NAs

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,15 +1,33 @@
 # Author: Elisabeth Nesmith
 # Title: Data Cleaning
-# Purpose: Loading data, selecting variables, and cleaning data
+# Purpose: Loading and cleaning data, creating basic visualizations
 
 # Load necessary packages
 library(data.table)
 library(tidyverse)
 
-# Load data as data tables
-yrbs_qn <- as.data.table(read.csv("Data/2019_YRBS_qn_variables.csv"))
-yrbs_q <- as.data.table(read.csv("Data/2019_YRBS_q_variables.csv"))
+# This data is from the 2020-2021 Healthy Minds Study. The data I pull in here is a subset of the original dataset, which contained
+# over 2000 variables. This subset only contains gender, race, age, and financial variables, as well as three different indicators of
+# student wellbeing and belonging.
 
-# Merge q variables with qn variables with key variables
-keys <- c("site", "raceeth", "q6orig", "q7orig", "record", "orig_rec", "q1", "q2", "q3", "q4", "q5", "q6", "q7")
-yrbs_2019 <- merge(yrbs_qn, yrbs_q, by = keys, all = TRUE)
+# Load data as data tables
+hms_data <- (read.csv("Data/hms_2021_data_subset.csv"))
+
+# check for NA categories
+no_gender <- hms_data %>% 
+  filter(is.na(gender_female) & is.na(gender_male) & is.na(gender_transm) & is.na(gender_transf) & is.na(gender_queernc) &
+           is.na(gender_nonbin) & is.na(gender_selfID))
+hms_data %>% 
+  filter(is.na(aca_impa))
+
+hms_data %>% 
+  filter(is.na(discrim))
+
+hms_data %>% 
+  filter(is.na(belong1))
+
+# remove rows with NA gender and NA academic impact (academic impact has fewest number of NAs of three response variables)
+
+
+
+
